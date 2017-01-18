91a257946b907951166ec43f842e5e1f12f467ac
Piris-Zazo <Piris-Zazo@DESKTOP-3OKJPBK>
2017-01-18 18:47:16 +0100

    Add project files.

---
+++ PruebaNavegacionCasa/PruebaNavegacionCasa/Vistas/Inicio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PruebaNavegacionCasa.Vistas
+{
+    public class Inicio: ContentPage
+    {
+        public Inicio()
+        {
+            var etiqueta = new Label
+            {
+                Text = "Pagina de inicio"
+            };
+
+            var btnCambiarPagina = new Button
+            {
+                Text = "Click Me"
+            };
+
+            btnCambiarPagina.Clicked += (sender, args) =>
+            {
+                Navigation.PushAsync(new Navegue());
+            };
+
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                Children = { etiqueta,btnCambiarPagina}
+                
+            };
+        }
+    }
+}
---
+++ PruebaNavegacionCasa/PruebaNavegacionCasa/Vistas/Navegue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PruebaNavegacionCasa.Vistas
+{
+    public class Navegue:ContentPage
+    {
+        public Navegue()
+        {                        
+            Content = new Label {Text = "Ehhh conseguimos navegar a otra pagina"};
+        }
+    }
+}
